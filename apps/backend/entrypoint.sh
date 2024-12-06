badb70e28de14b86fe30bdf1aa376584284d0686
AnimeshKotka-VaraTech <animesh.kotka@infrovate.com>
2024-12-06 12:39:48 +0530

    prod .changes added

---
+++ apps/backend/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# DB_SERVICE="chainscribe-db"
+
+# if [ "$NODE_ENV" == "test" ]; then
+#   DB_SERVICE="inv-test-db"
+# fi
+ls -lrt
+npm run labourize:migration:run \
+  && npm run start:prod
+ls -lrt
+  # && sh /bin/wait-for $DB_SERVICE:5432 -- npm run chainscribe:seeds:run \
+# npm run start:debug
